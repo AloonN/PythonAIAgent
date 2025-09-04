047de3a7666ef1ea647894d223b25f5c22443002
Alan Ngo <alan.t.ngo1st@gmail.com>
2025-09-04 18:38:06 -0400

    Removed some comments

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -1,13 +1,5 @@
 import os
 from google.genai import types
-# os.path.abspath(): Get an absolute path from a relative path
-# os.path.join(): Join two paths together safely (handles slashes)
-# .startswith(): Check if a string starts with a substring
-# os.path.isdir(): Check if a path is a directory
-# os.listdir(): List the contents of a directory
-# os.path.getsize(): Get the size of a file
-# os.path.isfile(): Check if a path is a file
-# .join(): Join a list of strings together with a separator
 
 schema_get_files_info = types.FunctionDeclaration(
     name="get_files_info",
